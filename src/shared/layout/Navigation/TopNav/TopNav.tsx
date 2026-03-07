93cdb9ac20a63f56e0ea74bffc530d5e93c0ff1d
Clement Jeulin <clement93jeulin@gmail.com>
2026-03-07 12:47:06 +0100

    feat(topNav) : CSS improvements

--- src/shared/layout/Navigation/TopNav/TopNav.tsx
+++ src/shared/layout/Navigation/TopNav/TopNav.tsx
@@ -1,15 +1,27 @@
-import { NavLink } from "react-router-dom";
+import  { NavLink } from "react-router-dom";
 import styles from "./TopNav.module.css"
 
-export default function TopNav(){
-    return  <nav className={styles.topNav}>
-                <ul>
-                    <li>
-                        <NavLink to={"/dashboard"} className={styles.link}>Dashboard</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={"/settings"} className={styles.link}>Settings</NavLink>
-                    </li>
-                </ul>
-            </nav>
+
+export default function TopNav() {
+  return (
+    <nav className={styles.topNav}>
+      <a href="/" className={styles.logo}>Dashboard React-TS</a>
+      <ul className={styles.ul}>
+        <li>
+          <NavLink to="/dashboard" className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.active : ""}`
+          }>
+            Dashboard
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/settings" className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.active : ""}`
+          }>
+            Settings
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
